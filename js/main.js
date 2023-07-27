b28cbc7d663c2073b233030a354f801ffaef15d4
ErnestKnowing <e3050901@gmail.com>
2023-07-27 16:57:28 +0300

    feat: menu appearance

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 (function () {
   const header = document.querySelector(".header");
-  window.addEventListener('scroll', function () {
+  window.addEventListener("scroll", function () {
     const scrollPosition = window.scrollY;
-    if(scrollPosition > 200){
-      header.classList.add('header_active')
+    if (scrollPosition > 200) {
+      header.classList.add("header_active");
     }
-    if(scrollPosition < 200){
-      header.classList.remove('header_active')
+    if (scrollPosition < 200) {
+      header.classList.remove("header_active");
     }
   });
 }());
+(function(){
+  const burger = document.querySelector(".burger");
+  burger.addEventListener("click", function () {
+    const menu = document.querySelector(".header_nav");
+    menu.classList.add("header_nav_active");
+  });
+}())
+
